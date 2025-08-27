83ae0268f89fea5e1499818e068b3fb3723b0e9c
Rafasantt <rafael_santos.s1@hotmail.com>
2025-08-26 23:08:50 -0300

    chore: AJustes na porta de exposição, mudança da 5174 para 5173.

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     // Configura o servidor para ouvir em 0.0.0.0
     host: true,
-    port: 5174,
+    port: 5173,
     watch: {
       usePolling: true,
     }
